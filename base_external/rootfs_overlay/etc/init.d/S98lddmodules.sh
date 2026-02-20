0486fd4a5c70d897dd3f95f7ab5e6069258ae725
Alan Cano <cano.alan1794@gmail.com>
2026-02-20 15:46:21 -0600

    Adding init scripts for loading and unloadingkernel modules using root filesystem overlay method

---
+++ base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# This script is used to load the ldd modules at boot time. 
+# It is placed in the rootfs_overlay directory and will be copied to the root filesystem during the build process.
+
+case "$1" in
+start)
+echo "Starting init script for Module Loading"
+scull_load
+module_load faulty
+modprobe hello
+;;
+stop)
+echo "Removing user modules"
+rmmod hello
+module_unload faulty
+scull_unload
+;;
+*)
+echo "Usage: $0 {start|stop}"
+exit 1
+esac
+exit 0
